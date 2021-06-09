2f9acc636ac1342707cfb8eadd902fd5522a1e4c
Katy Currie <katylcurrie@gmail.com>
2021-06-09 13:59:34 +0100

    First commit

---
+++ src/main/java/com/GrowHub/Server/ServerApplication.java
@@ -0,0 +1,13 @@
+package com.GrowHub.Server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerApplication.class, args);
+	}
+
+}

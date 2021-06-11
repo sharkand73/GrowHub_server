83b5619e917e2a0542b78610e25556a34c254d0d
Katy Currie <katylcurrie@gmail.com>
2021-06-11 13:40:42 +0100

    Extra column added to User class

--- src/main/java/com/GrowHub/Server/models/User.java
+++ src/main/java/com/GrowHub/Server/models/User.java
@@ -1,41 +1,30 @@
 package com.GrowHub.Server.models;
-
 import com.GrowHub.Server.models.enums.PositionType;
-import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import org.hibernate.annotations.Cascade;
-
 import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
-
 @Entity
 @Table(name="users")
 public class User {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
-
     @Column
     private String shortName;
-
     @Column
     private String email;
-
     @Column
     private String password;
-
     @Column
-    @Enumerated(value = EnumType.STRING)
     private PositionType position;
-
+    @Column
+    private boolean onCommittee;
     @Column
     private int yearJoined;
-
     @Column
     private int yearLeft;
-
     @JsonIgnoreProperties(value = "users")
     @ManyToMany
     @Cascade(org.hibernate.annotations.CascadeType.SAVE_UPDATE)
@@ -48,7 +37,6 @@ public class User {
             }
     )
     private List<Plot> plots;
-
     @JsonIgnoreProperties(value = "users")
     @ManyToMany
     @Cascade(org.hibernate.annotations.CascadeType.SAVE_UPDATE)
@@ -61,7 +49,6 @@ public class User {
             }
     )
     private List<Job> jobs;
-
     @JsonIgnoreProperties(value = "users")
     @ManyToMany
     @Cascade(org.hibernate.annotations.CascadeType.SAVE_UPDATE)
@@ -74,123 +61,90 @@ public class User {
             }
     )
     private List<Crop> crops;
-
-    @JsonIgnoreProperties(value = "author")
-    @OneToMany(mappedBy = "author", fetch = FetchType.LAZY)
-    private List<TextContent> textContents;
-
-
     //CONSTRUCTOR
     public User(String shortName, String email, String password, PositionType position, int yearJoined) {
         this.shortName = shortName;
         this.email = email;
         this.password = password;
         this.position = position;
+        this.onCommittee = isOnCommittee();
         this.yearJoined = yearJoined;
         this.yearLeft = 0;
         this.crops = new ArrayList<>();
         this.plots = new ArrayList<>();
         this.jobs = new ArrayList<>();
-        this.textContents = new ArrayList<>();
         this.id = id;
     }
-
     public User() {
     }
-
-
-
     public String getShortName() {
         return shortName;
     }
-
     public void setShortName(String shortName) {
         this.shortName = shortName;
     }
-
     public String getEmail() {
         return email;
     }
-
     public void setEmail(String email) {
         this.email = email;
     }
-
     public String getPassword() {
         return password;
     }
-
     public void setPassword(String password) {
         this.password = password;
     }
-
     public PositionType getPosition() {
         return position;
     }
-
     public void setPosition(PositionType position) {
         this.position = position;
     }
-
+    public boolean getOnCommittee() {
+        return onCommittee;
+    }
+    public void setOnCommittee(boolean onCommittee) {
+        this.onCommittee = onCommittee;
+    }
     public int getYearJoined() {
         return yearJoined;
     }
-
     public void setYearJoined(int yearJoined) {
         this.yearJoined = yearJoined;
     }
-
     public int getYearLeft() {
         return yearLeft;
     }
-
     public void setYearLeft(int yearLeft) {
         this.yearLeft = yearLeft;
     }
-
     public List<Crop> getCrops() {
         return crops;
     }
-
     public void setCrops(List<Crop> crops) {
         this.crops = crops;
     }
-
     public List<Plot> getPlots() {
         return plots;
     }
-
     public void setPlots(List<Plot> plots) {
         this.plots = plots;
     }
-
     public List<Job> getJobs() {
         return jobs;
     }
-
     public void setJobs(List<Job> jobs) {
         this.jobs = jobs;
     }
-
+    // add/remove job functions?
+    public boolean isOnCommittee() {
+        return !(position == PositionType.NONE || position == PositionType.INACTIVE);
+    }
     public Long getId() {
         return id;
     }
-
     public void setId(Long id) {
         this.id = id;
-    }
-
-    public List<TextContent> getTextContents() {
-        return textContents;
-    }
-
-    public void setTextContents(List<TextContent> textContents) {
-        this.textContents = textContents;
-    }
-
-    // add/remove job functions?
-
-    public boolean isOnCommittee() {
-        return !(position == PositionType.NONE || position == PositionType.INACTIVE);
     }
 }

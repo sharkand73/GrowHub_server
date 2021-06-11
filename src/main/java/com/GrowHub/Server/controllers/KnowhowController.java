0a4fff8132a982d2c29ceef27757ba7e276b8c65
Andrew Sharkey <sharkand@hotmail.com>
2021-06-11 16:00:41 +0100

    Knowhow controller created.

---
+++ src/main/java/com/GrowHub/Server/controllers/KnowhowController.java
@@ -0,0 +1,22 @@
+package com.GrowHub.Server.controllers;
+
+import com.GrowHub.Server.repositories.BulletinItemRepository;
+import com.GrowHub.Server.repositories.KnowhowRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class KnowhowController {
+
+    @Autowired
+    KnowhowRepository knowhowRepository;
+
+    @GetMapping(value = "/knowhows")
+    public ResponseEntity getAllKnowhows() {
+        return new ResponseEntity(knowhowRepository.findAll(), HttpStatus.OK);
+    }
+
+}

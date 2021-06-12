9f4a11a739e25bd5bc21684ce2325c483222e36d
Andrew Sharkey <sharkand@hotmail.com>
2021-06-12 20:47:55 +0100

    Cannot find users by plot number.  Test failing

--- src/test/java/com/GrowHub/Server/ServerApplicationTests.java
+++ src/test/java/com/GrowHub/Server/ServerApplicationTests.java
@@ -9,6 +9,9 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
@@ -64,6 +67,26 @@ class ServerApplicationTests {
 		plot1.addUser(Andy);
 		plot1.removeUser(Mike);
 		assertEquals(1, plot1.getUsers().size());
+	}
+
+	@Test
+	void canFindUsersByPlotNumber() {
+		User Andy = new User("AndyS", "soilmuncher@hotmail.co.uk", "jobbie101", PositionType.TREASURER, 2017);
+		userRepository.save(Andy);
+		User Pauline = new User("PaulineB", "paulinelovescabbage@gmail", "whizkid763", PositionType.ORDINARY, 2016);
+		userRepository.save(Pauline);
+		User Brian = new User("BrianH", "mrlovemonster@gmail", "45lovemachine78", PositionType.ORDINARY, 2020);
+		userRepository.save(Brian);
+
+		Plot plot1 = new Plot("Plot 1", 1, 5.4, 4.5, true);
+		plot1.addUser(Andy);
+		plot1.addUser(Pauline);
+		plot1.addUser(Brian);
+		plotRepository.save(plot1);
+
+		List<User> foundUsers = userRepository.findByPlotsPlotNumber(1);
+		assertEquals(3, foundUsers.size());
+
 	}
 
 }

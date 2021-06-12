a43da6a1f919d3232cb54e8fdf1d8866f374c0c8
Andrew Sharkey <sharkand@hotmail.com>
2021-06-12 19:52:36 +0100

    First plot test failing. Need to debug.

--- src/main/java/com/GrowHub/Server/repositories/PlotRepository.java
+++ src/main/java/com/GrowHub/Server/repositories/PlotRepository.java
@@ -4,6 +4,10 @@ import com.GrowHub.Server.models.Plot;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface PlotRepository extends JpaRepository<Plot, Long> {
+
+    List<Plot> findByUsersShortName(String name);
 }
--- src/test/java/com/GrowHub/Server/ServerApplicationTests.java
+++ src/test/java/com/GrowHub/Server/ServerApplicationTests.java
@@ -1,13 +1,33 @@
 package com.GrowHub.Server;
 
+import com.GrowHub.Server.models.Plot;
+import com.GrowHub.Server.models.User;
+import com.GrowHub.Server.models.enums.PositionType;
+import com.GrowHub.Server.repositories.PlotRepository;
+import com.GrowHub.Server.repositories.UserRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class ServerApplicationTests {
 
+	@Autowired
+	UserRepository userRepository;
+
+	@Autowired
+	PlotRepository plotRepository;
+
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	void plotsStartOffEmpty() {
+		Plot plot1 = new Plot("Plot 1", 1, 5.4, 4.5, true);
+		assertEquals(0, plot1.getUsers().size());
 	}
 
 }
